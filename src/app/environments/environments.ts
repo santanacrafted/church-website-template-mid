44877b2955638570a4909cc2ab212202e6667957
Darren Santana <dcodexperience@gmail.com>
2025-05-16 14:08:51 -0400

    fix

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: 'AIzaSyABLtKinRN0QECbmqUmYcwY712AK3QFxmc',
-    authDomain: 'church-app-template-64ef2-2c470.firebaseapp.com',
-    projectId: 'church-app-template-64ef2-2c470',
-    storageBucket: 'church-app-template-64ef2-2c470.firebasestorage.app',
+    authDomain: 'church-app-template-64ef2.firebaseapp.com',
+    projectId: 'church-app-template-64ef2',
+    storageBucket: 'church-app-template-64ef2.firebasestorage.app',
     messagingSenderId: '570171072726',
     appId: '1:570171072726:web:a3673708856d74f6426977',
     measurementId: 'G-6HM4P606V7',
